4597b308aac7382c79602fe00ec7411e8c11b9dd
Araceli Ramos <celi2455@gmail.com>
2022-05-10 15:19:09 -0500

    Completed assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+    end
+
+    def grade(year)
+        @roster[year]
+    end
+
+    def sort
+        @roster.each do |s,g|
+            g.sort!
+        end
+    end
+    
+end
